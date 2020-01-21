9d588abda85ef126f7c6c40ceaedfc695adbd21e
Jody <jodyvermeulen007@gmail.com>
2020-01-21 11:09:59 +0200

    Configure server for authentication

---
+++ tserver/dist/config/index.js
@@ -0,0 +1,6 @@
+"use strict";
+var LINKEDIN = {
+    clientID: "86g059et8y7aor",
+    clientSecret: "EPDAbpMZaLiqdKTP"
+};
+module.exports = { LINKEDIN: LINKEDIN };
--- tserver/index.ts
+++ tserver/index.ts
@@ -1,13 +1,52 @@
 import express from "express";
 import { PORT } from "./src/config/constants";
 
-const bodyParser = require("body-parser");
 const path = require("path");
 const app = express();
+const cors = require("cors");
+const passport = require("passport");
+const LinkedInStrategy = require("passport-linkedin");
+const chalk = require("chalk");
+let user = {};
+
+//!----------------LinkedIn Strategy---------------------!
+passport.use(
+  new LinkedInStrategy(
+    {
+      consumerKey: "86g059et8y7aor",
+      consumerSecret: "EPDAbpMZaLiqdKTP",
+      callbackUrl: "/auth/linkedin/callback"
+    },
+    (accessToken, refreshToken, profile, cb) => {
+      console.log(chalk.blue(JSON.stringify(profile)));
+      user = { ...profile };
+      return cb(null, profile);
+    }
+  )
+);
+
+app.use(cors());
+app.use(passport.initialize());
+app.get("/auth/linkedin", passport.authenticate("linkedin"));
+app.get(
+  "/auth/linkedin/callback",
+  passport.authenticate("linkedin", (req, res) => {
+    res.redirect("/profile");
+  })
+);
+
+//!----------------Default---------------------!
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get("/ping", function(req, res) {
-  return res.send("pong");
+app.get("/user", function(req, res) {
+  console.log("getting user data!");
+  return res.send(user);
+});
+
+app.get("/auth/logout", function(req, res) {
+  console.log("logging out!");
+  user = {};
+  return res.redirect("/");
 });
 
 app.get("/", function(req, res) {
