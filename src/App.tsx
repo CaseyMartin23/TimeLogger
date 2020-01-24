c0652505e81e081d770eb4f1e53d61132c17c695
JodyVermeulen <48580977+Jody328@users.noreply.github.com>
2020-01-24 13:17:38 +0200

    Implement landing page (WIP)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import "./App.css";
-import { Login } from "./pages/Login";
+// import { Login } from "./pages/Login";
+import { Home } from "./pages/Home";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "semantic-ui-css/semantic.css";
 
 const App: React.FC = () => {
   return (
     <Router>
       <Switch>
-        <Route path="/" component={Login} />
+        {/* <Route path="/" component={Login} /> */}
+        <Route path="/" component={Home} />
       </Switch>
     </Router>
   );
