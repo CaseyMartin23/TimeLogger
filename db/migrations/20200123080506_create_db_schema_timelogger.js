aef8aa1a31e251774e220c0703654a7045da7640
CaseyMartin23 <cm232311@gmail.com>
2020-02-03 08:42:04 +0200

    Database and User Info displaying ...

--- db/migrations/20200123080506_create_db_schema_timelogger.js
+++ db/migrations/20200123080506_create_db_schema_timelogger.js
@@ -1,13 +1,4 @@
 exports.up = function(knex) {
-<<<<<<< HEAD
-  return knex.schema.createTable("users", function(table) {
-    table.increments("user_id", [80]).notNullable();
-    table.string("LinkedinId", [100]).notNullable();
-    table.string("Username", [150]).notNullable();
-    table.string("Firstname", [100]).notNullable();
-    table.string("Lastname", [100]).notNullable();
-    table.string("UserRole", [15]);
-=======
   return knex.schema.createTable("users", table => {
     table.increments("user_id").primary();
     table.string("LinkedinId").notNullable();
@@ -15,7 +6,6 @@ exports.up = function(knex) {
     table.string("Firstname").notNullable();
     table.string("Lastname").notNullable();
     table.string("UserRole");
->>>>>>> 39fd694358f46a2a90784590a5e30306a2769232
     table.timestamp("created_at").defaultTo(knex.fn.now());
     table.timestamp("updated_at").defaultTo(knex.fn.now());
   });
