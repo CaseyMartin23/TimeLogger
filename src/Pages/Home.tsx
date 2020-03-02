c4891ba2234d3426ce2b76f7ecbfbd627ab38b15
CaseyMartin23 <cm232311@gmail.com>
2020-03-02 12:06:59 +0200

    Cleaned up login page

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -8,9 +8,13 @@ export const Home: React.FC = () => {
 
   const getUserData = async () => {
     setLoaded(false);
-    await fetch("/whoami")
-      .then(res => res.json())
-      .then(data => setUserData(data));
+    try {
+      await fetch("/whoami")
+        .then(res => res.json())
+        .then(data => setUserData(data));
+    } catch (e) {
+      console.log(e);
+    }
     setLoaded(true);
   };
 
@@ -22,14 +26,14 @@ export const Home: React.FC = () => {
     }
   }, [loaded]);
 
-  if (!userData) return <Redirect to="/login" />;
-
   if (!loaded) return <div>Loading ...</div>;
+
+  if (!userData) return <Redirect to="/login" />;
 
   return (
     <div>
-      This is the home page
-      <Button href="http://localhost/auth/logout">Log out</Button>
+      <div>This is the home page</div>
+      <Button href="http://localhost:3005/auth/logout">Log out</Button>
     </div>
   );
 };
